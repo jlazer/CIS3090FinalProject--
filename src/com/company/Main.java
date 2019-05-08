0def254f7b10cdaf4b489f12e17507b05568504c
Justin Lazarski <blackhawks189@gmail.com>
2019-05-08 14:46:27 -0700

    Initial commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+//Justin Lazarski
+//CIS 3090.04
+//5/8/19
+//Final Project
+
+
+
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+
+
+        System.out.println("=== Select Sandwich Bread ===");
+        System.out.println("1 White Bread $1.5");
+        System.out.println("2 Wheat Bread $1.6");
+        System.out.println("3 French Bread $1.8");
+        System.out.println("4 Organic Bread $2.0");
+        System.out.println("Select a bread [1 - 4]:");
+        choice = input.nextInt();
+
+
+        System.out.println(choice);
+
+
+
+
+
+
+
+
+    }
+}
